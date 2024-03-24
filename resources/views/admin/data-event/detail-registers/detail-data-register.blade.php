a3d1d420ae42d8b25592eb28d6b9b8e3eb68979b
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-03-24 15:48:48 +0700

    fix(detail data register): fix font weight

--- resources/views/admin/data-event/detail-registers/detail-data-register.blade.php
+++ resources/views/admin/data-event/detail-registers/detail-data-register.blade.php
@@ -23,41 +23,41 @@
     <div class="btn-wrapper mt-2">
     </div>
     <div class="mb-3">
-      <p class="form-label">Nama Lengkap : </p>
-      <p class="fw-semibold">{{$register->name}}</p>
+      <p class="form-label fw-semibold">Nama Lengkap : </p>
+      <p class="">{{$register->name}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">NIM : </p>
-      <p class="fw-semibold">{{$register->nim}}</p>
+      <p class="form-label fw-semibold">NIM : </p>
+      <p class="">{{$register->nim}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">NIK : </p>
-      <p class="fw-semibold">{{$register->nik}}</p>
+      <p class="form-label fw-semibold">NIK : </p>
+      <p class="">{{$register->nik}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">Jurusan : </p>
-      <p class="fw-semibold">{{$register->departement}}</p>
+      <p class="form-label fw-semibold">Jurusan : </p>
+      <p class="">{{$register->departement}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">Program Studi : </p>
-      <p class="fw-semibold">{{$register->program_study}}</p>
+      <p class="form-label fw-semibold">Program Studi : </p>
+      <p class="">{{$register->program_study}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">Semester : </p>
-      <p class="fw-semibold">{{$register->semester}}</p>
+      <p class="form-label fw-semibold">Semester : </p>
+      <p class="">{{$register->semester}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">Email : </p>
-      <p class="fw-semibold">{{$register->email}}</p>
+      <p class="form-label fw-semibold">Email : </p>
+      <p class="">{{$register->email}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">No. Wa : </p>
-      <p class="fw-semibold">{{$register->phone_num}}</p>
+      <p class="form-label fw-semibold">No. Wa : </p>
+      <p class="">{{$register->phone_num}}</p>
     </div>
     <div class="mb-3">
-      <p class="form-label">Foto KTP : </p>
+      <p class="form-label fw-semibold">Foto KTP : </p>
       <a href="{{route('admin.data.registers.downloadKTP', $register->ktp_img)}}">
-        <p class="fw-semibold">{{$register->ktp_img}}</p>
+        <p class="">{{$register->ktp_img}}</p>
       </a>
       <div class="img-wrapper col-lg-6 col-10">
         <img src="{{ asset('storage/ktp/'.$register->ktp_img) }}" alt="ktp" class="img-fluid w-100 register-data-img">
@@ -65,9 +65,9 @@
       </div>
     </div>
     <div class="mb-3">
-      <p class="form-label">Foto KTM : </p>
+      <p class="form-label fw-semibold">Foto KTM : </p>
       <a href="{{route('admin.data.registers.downloadKTM', $register->ktm_img)}}">
-        <p class="fw-semibold">{{$register->ktm_img}}</p>
+        <p class="">{{$register->ktm_img}}</p>
       </a>
       <div class="img-wrapper col-lg-6 col-10">
         <img src="{{ asset('storage/ktp/'.$register->ktp_img) }}" alt="ktp" class="img-fluid w-100 register-data-img">
@@ -75,21 +75,21 @@
       </div>
     </div>
     <div class="mb-3">
-      <p class="form-label">Surat Pernyataan Nominasi IISMA : </p>
+      <p class="form-label fw-semibold">Surat Pernyataan Nominasi IISMA : </p>
       <a class="text-decoration-none"
         href="{{route('admin.data.registers.downloadSuratPernyataan', [$register->surat_pernyataan_iisma, 0])}}">
-        <p class="fw-semibold btn btn-danger">Download PDF</p>
+        <p class=" btn btn-danger">Download PDF</p>
       </a>
       <a class="text-decoration-none" target="blank"
         href="{{route('admin.data.registers.downloadSuratPernyataan', [$register->surat_pernyataan_iisma, 1])}}">
-        <p class="fw-semibold btn btn-success ms-2">View PDF</p>
+        <p class=" btn btn-success ms-2">View PDF</p>
       </a>
 
     </div>
     <div class="mb-5">
-      <p class="form-label">Pas Foto : </p>
+      <p class="form-label fw-semibold">Pas Foto : </p>
       <a href="{{route('admin.data.registers.downloadPasFoto', $register->pasFoto_img)}}">
-        <p class="fw-semibold">{{$register->pasFoto_img}}</p>
+        <p class="">{{$register->pasFoto_img}}</p>
       </a>
       <div class="img-wrapper col-lg-6 col-10">
         <img src="{{ asset('storage/pasFoto/'.$register->pasFoto_img) }}" alt="ktp"
