01cefd9c7fcfd5b273589f7d9275030cd83851ca
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-07-11 19:22:36 +0700

    fix: fix bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\MailSetting;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,21 +22,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $mailsetting = MailSetting::first();
-        if ($mailsetting) {
-            $data = [
-                'driver'            => $mailsetting->mail_transport,
-                'host'              => $mailsetting->mail_host,
-                'port'              => $mailsetting->mail_port,
-                'encryption'        => $mailsetting->mail_encryption,
-                'username'          => $mailsetting->mail_username,
-                'password'          => $mailsetting->mail_password,
-                'from'              => [
-                    'address' => $mailsetting->mail_from,
-                    'name'   => 'LaravelStarter'
-                ]
-            ];
-            Config::set('mail', $data);
+        if (Schema::hasTable('nama_tabel')) {
+            $mailsetting = MailSetting::first();
+            if ($mailsetting) {
+                $data = [
+                    'driver'            => $mailsetting->mail_transport,
+                    'host'              => $mailsetting->mail_host,
+                    'port'              => $mailsetting->mail_port,
+                    'encryption'        => $mailsetting->mail_encryption,
+                    'username'          => $mailsetting->mail_username,
+                    'password'          => $mailsetting->mail_password,
+                    'from'              => [
+                        'address' => $mailsetting->mail_from,
+                        'name'   => 'LaravelStarter'
+                    ]
+                ];
+                Config::set('mail', $data);
+            }
         }
     }
 }
