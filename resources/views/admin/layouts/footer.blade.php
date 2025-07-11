f91a8c07f986c08845fa6b05c2a97c1b77a1c6df
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-07-11 20:15:10 +0700

    update

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="footer-wrapper fixed-bottom d-flex justify-content-center text-secondary ">
-  <strong>Copyright ©{{date('Y')}}, UPA BAHASA POLINEMA</strong>
+  <strong>Copyright ©{{date('Y')}} UPA BAHASA POLINEMA</strong>
 </div>
